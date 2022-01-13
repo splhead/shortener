95159b5a952910adce84c5b6bbe1f1024b43dd01
Silas P Ladislau <silas.ladislau@ale.ro.gov.br>
2022-01-13 12:56:15 -0400

    add files .gitignore

---
+++ prisma/migrations/20220113125602_create_ur_ls/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Urls" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "hash" TEXT NOT NULL,
+    "shortURL" TEXT NOT NULL,
+    "originURL" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
